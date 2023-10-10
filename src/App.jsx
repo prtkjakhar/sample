51c95f4fa04285df16dd7916f59213235142172e
prtkjakhar <prtkjakhar@gmail.com>
2023-10-10 18:03:03 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
 
   const handleClick = () => {
     // eslint-disable-next-line no-undef
+    if(shareUrl){
+      alert("shareUrl found")
+    }else if(window.shareUrl){
+      alert("window.shareUrl found")
+    }
+    // eslint-disable-next-line no-undef
     shareUrl.postMessage("Button clicked");
   }
 
